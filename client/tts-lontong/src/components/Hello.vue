dfb512d366deeadef3784b912a0a792907c754be
fajarkarim <fajarabdulkarim@gmail.com>
2017-07-14 10:56:16 +0700

    added answer check

--- client/tts-lontong/src/components/Hello.vue
+++ client/tts-lontong/src/components/Hello.vue
@@ -4,13 +4,11 @@
       <tr v-for="i in 5">
         <td v-for="j in 5">
           <input v-if="crossword[i-1][j-1] == ' '" type="text" name="" :value="crossword[i-1][j-1]" disabled>
-          <input v-else type="text" name="" v-model="crossword[i-1][j-1]" @keyup="updateBoard(i-1, j-1, value)">
+          <input v-else type="text" name="" v-model="crossword[i-1][j-1]">
         </td>
       </tr>
     </table>
-    <input type="text" name="" v-model="halo">
-    <input type="text" @>
-    <h2>{{ halo }}</h2>
+    <button type="button" name="button" @click="checkAll">Anda Yakin ?</button>
   </div>
 </template>
 
@@ -22,15 +20,11 @@ export default {
       msg: 'Welcome to Your Vue.js App',
       halo: 'coba',
       updatedValue: '',
-      dict: [
-        [' ', 'M', 'A', 'N', 'A'],
-        [' ', 'A', ' ', ' ', ' '],
-        ['A', 'K', 'U', ' ', ' '],
-        [' ', 'A', ' ', ' ', ' '],
-        [' ', 'N', 'A', 'S', 'I']
-      ],
+      hAnswer: [[2, 0], [4, 1]],
+      vAnswer: [[0, 1]],
+      answer: ['MANA', 'MAKAN', 'AKU', 'NASI'],
       crossword: [
-        [' ', 'M', '', 'N', 'A'],
+        [' ', 'M', 'A', 'N', 'A'],
         [' ', '', ' ', ' ', ' '],
         ['A', '', 'U', ' ', ' '],
         [' ', '', ' ', ' ', ' '],
@@ -41,6 +35,37 @@ export default {
   methods: {
     updateBoard: function (i, j, val) {
       console.log(`${i}, ${j}, ${val}`)
+    },
+    checkAll: function () {
+      for (let i = 0; i < this.hAnswer.length; i++) {
+        console.log(this.checkHorizontal(this.hAnswer[i]))
+      }
+      for (let i = 0; i < this.vAnswer.length; i++) {
+        console.log(this.checkVertikal(this.vAnswer[i]))
+      }
+    },
+    checkHorizontal: function (pos) {
+      let r = pos[0]
+      let word = this.crossword[r].join('').trim()
+      if (this.answer.includes(word)) {
+        return ['horizontal', true, word]
+      } else {
+        return ['horizontal', false, word]
+      }
+    },
+    checkVertikal: function (pos) {
+      let c = pos[1]
+      let hWord = []
+      let word
+      for (let i = 0; i < this.crossword.length; i++) {
+        hWord.push(this.crossword[i][c])
+      }
+      word = hWord.join('').trim()
+      if (this.answer.includes(word)) {
+        return ['vertikal', true, word]
+      } else {
+        return ['vertikal', false, word]
+      }
     }
   }
 }
