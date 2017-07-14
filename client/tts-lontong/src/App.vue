8dc3a0379f2b71b67daefff777d112e75671ea02
fajarkarim <fajarabdulkarim@gmail.com>
2017-07-14 15:48:46 +0700

    last for merge

--- client/tts-lontong/src/App.vue
+++ client/tts-lontong/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -15,7 +17,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: white;
+  font-weight: bold;
 }
 </style>
--- client/tts-lontong/src/components/Hello.vue
+++ client/tts-lontong/src/components/Hello.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="hello">
-    <table>
-      <tr v-for="i in 5">
-        <td v-for="j in 5">
-          <input v-if="crossword[i-1][j-1] == ' '" type="text" name="" :value="crossword[i-1][j-1]" maxlength="1" size="4" disabled>
-          <input v-else type="text" name="" maxlength="1" size="10" v-model="crossword[i-1][j-1]">
-        </td>
-      </tr>
-    </table>
-    <button type="button" name="button" @click="checkAll">Anda Yakin ?</button>
-    <!-- <img src="../assets/background.jpg" alt="" width="100%"></img> -->
-    <!-- <img src="./assets/logo.png"> -->
-
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+        <table class="board">
+          <tr v-for="i in 5">
+            <td v-for="j in 5">
+              <input v-if="crossword[i-1][j-1] == ' '" type="text" name="" :value="crossword[i-1][j-1]" maxlength="1" size="4" disabled>
+              <input v-else type="text" name="" maxlength="1" size="10" v-model="crossword[i-1][j-1]">
+            </td>
+          </tr>
+        </table>
+        <div class="row">
+          <div class="col-md-4 col-md-offset-3">
+            <div id="question">
+              <h3> {{ currentQuestion }}</h3>
+            </div>
+            <div id="choice">
+              <h1>Anda yakin ???</h1>
+              <button type="button" name="button" @click="checkAll">Pasti !</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +33,10 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
       halo: 'coba',
+      currentQuestion: 'sss',
+      listQuestions: [
+        `Badan Usaha Milik Negara disingkat...`
+      ],
       correctHAnswer: '',
       correctVAnswer: '',
       updatedValue: '',
@@ -129,9 +144,24 @@ input[disabled] {
   background-color: black;
 }
 
+table {
+  padding: 5px;
+  background-color: black;
+}
+
 .hello {
   padding: 100px;
   background-image: url('http://jurnalapps.co.id/assets/img/content/1491636977_TTS-Lontong.jpg');
   height: 100%;
+}
+
+#question {
+  padding: 0.5em;
+  background-color: black;
+}
+
+#choice {
+  padding: 0,5px;
+  background-color: black;
 }
 </style>
